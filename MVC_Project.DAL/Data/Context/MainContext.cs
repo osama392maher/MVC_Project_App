67a1baff607c87acb80d3fa620179d720437a098
osama <osama392maher@gmail.com>
2024-03-26 23:45:15 +0200

    DAL for Departement Module

---
+++ MVC_Project.DAL/Data/Context/MainContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_Project.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Project.DAL.Data.Context
+{
+    internal class MainContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlServer("Server = .; Database = MVC_Project; Trusted_Connection=True; MultipleActiveResultSets = True");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        public DbSet<Department> Departments { get; set; }
+    }
+}
---
+++ MVC_Project.DAL/Models/Department.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Project.DAL.Models
+{
+    internal class Department
+    {
+        public int Id { get; set; }
+
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime DateOFCreation { get; set; }
+
+
+
+    }
+}
