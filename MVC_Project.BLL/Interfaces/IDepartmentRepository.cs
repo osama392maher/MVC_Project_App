9433f01f0055b322397924444678ae3c92754df7
osama <osama392maher@gmail.com>
2024-03-27 02:21:53 +0200

    Department Repo Class and Interface

---
+++ MVC_Project.BLL/Interfaces/IDepartmentRepository.cs
@@ -0,0 +1,20 @@
+﻿using MVC_Project.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Project.BLL.Interfaces
+{
+    internal interface IDepartmentRepository
+    {
+        int Add(Department department);
+        int Update(Department department);
+        int Delete(Department department);
+
+        IEnumerable<Department> GetAll();
+
+        Department GetById(int id);
+    }
+}
---
+++ MVC_Project.BLL/Repositories/DepartmentRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_Project.BLL.Interfaces;
+using MVC_Project.DAL.Data.Context;
+using MVC_Project.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Project.BLL.Repositories
+{
+    internal class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly MainContext context;
+
+        public DepartmentRepository(MainContext mainContext)
+        {
+            this.context = mainContext;
+        }
+
+        public int Add(Department department)
+        {
+            context.Departments.Add(department);
+            return context.SaveChanges();
+        }
+
+        public int Update(Department department)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Delete(Department department)
+        {
+            context.Departments.Remove(department);
+            return context.SaveChanges();
+        }
+
+        public IEnumerable<Department> GetAll()
+        {
+            return context.Departments.AsNoTracking().ToList();
+        }
+
+        public Department GetById(int id)
+        {
+            //var department = context.Departments.Local.Where(d => d.Id == id).FirstOrDefault();
+            //if (department == null)
+            //	department = context.Departments.Where(d => d.Id == id).FirstOrDefault();
+            //return department;
+            return context.Departments.Find(id);
+        }
+    }
+}
